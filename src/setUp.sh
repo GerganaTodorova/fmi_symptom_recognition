71c019f1bbbb7848184d57a6f19e4d6d35cf6bec
GerganaTodorova <GerganaTodorova@users.noreply.github.com>
2021-03-01 21:42:17 +0200

    set up script

---
+++ src/setUp.sh
@@ -0,0 +1,18 @@
+pip install -U spacy==2.3.2
+
+pip install stanza==1.1.1 -U
+ 
+pip install spacy-stanza==0.2.4
+
+pip install import_ipynb
+
+pip install voila
+
+python -m pip install ipykernel
+python -m ipykernel install --user
+
+pip install pandas
+
+pip install ipywidgets
+
+python .\installStanzaForBg.py
